87261eed0bfc05f88f6c571062681cdf5957e787
Tasneem <tasneem123@gmail.com>
2026-03-28 11:35:17 +0530

    Generate Color

---
+++ activity.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click",function(){
+    let h2= document.querySelector("h2");
+    let randomColor = getRandomColor();
+    h2.innerText = randomColor;
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+    console.log("Color Updated");
+});
+
+function getRandomColor(){
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+
+}
